9606fb76184473e4def7f06ed216ef4424c3205b
sergk <sergey23144y@gmail.com>
2023-04-11 19:40:18 +0300

    new

--- main/ClassUser.py
+++
@@ -1,8 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class User(StatesGroup):
-    user_name = State()
-    user_email = State()
-    user_phon = State()
-    user_password = State()
-    user_score = State()
